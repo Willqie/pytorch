58df83c627b48d9263d438f1a4d8292617a22311
will <will.qie@mail.utoronto.ca>
2021-06-30 08:35:09 -0400

    ff

--- BMMExt/test.py
+++ BMMExt/test.py
@@ -4,13 +4,13 @@ import numpy as np
 
 if __name__ == "__main__":
     a = torch.rand((4, 128, 8)).cuda()
-    b = torch.rand((3, 3, 8)).cuda()
+    b = torch.rand((3, 8, 3)).cuda()
     s = torch.FloatTensor([256, 128, 128])
     res = torch.zeros((4, 128, 3)).cuda()
-    res = BMMExt.op(a, b.permute([0, 2, 1]), s, res, 4, 128)
-    res = BMMExt.op(a, b.permute([0, 2, 1]), s, res, 4, 128)
+    res = BMMExt.op(a, b, s, res, 4, 128)
+    res = BMMExt.op(a, b, s, res, 4, 128)
     
-    b_ = b.permute([0,2,1])
+    b_ = b
     bb = torch.empty((4, 8, 3)).cuda()
     bb[0] = b_[0]
     bb[1] = b_[0]
