f4c1554793f3c2e38050dea82c115565c97c941c
will <will.qie@mail.utoronto.ca>
2021-06-30 04:13:43 -0400

    ff

--- BMMExt/test.py
+++ BMMExt/test.py
@@ -1,5 +1,6 @@
 import torch
 import BMMExt
+import numpy as np
 
 if __name__ == "__main__":
     a = torch.randn((4, 16, 3)).cuda()
@@ -7,4 +8,22 @@ if __name__ == "__main__":
     c = torch.randn((3, 10)).cuda()
     s = torch.FloatTensor([16, 16, 32])
     res = torch.empty((4, 16, 10)).cuda()
-    print(BMMExt.forward(a, b, s, c, res, 4, 16))
+    BMMExt.forward(a, b, s, c, res, 4, 16)
+    
+    bb = torch.empty((4, 3, 10))
+    bb[0] = b[0]
+    bb[1] = b[1]
+    bb[2] = b[2]
+    bb[3] = b[2]
+    
+    cc = torch.empty((4, 10))
+    cc[0] = c[0]
+    cc[1] = c[1]
+    cc[2] = c[2]
+    cc[3] = c[2]
+    expected = torch.baddbmm(cc, a, bb)
+    np.testing.assert_allclose(
+                res.numpy().flatten(),
+                expected.numpy().flatten(),
+                rtol=1e-4
+    )
