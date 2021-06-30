42295ddf6588a441f380b701147f3c1d25d662db
will <will.qie@mail.utoronto.ca>
2021-06-30 08:33:57 -0400

    f

--- BMMExt/test.py
+++ BMMExt/test.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     res = BMMExt.op(a, b.permute([0, 2, 1]), s, res, 4, 128)
     
     b_ = b.permute([0,2,1])
-    bb = torch.empty((4, 3, 8)).cuda()
+    bb = torch.empty((4, 8, 3)).cuda()
     bb[0] = b_[0]
     bb[1] = b_[0]
     bb[2] = b_[1]
