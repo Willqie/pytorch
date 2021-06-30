4e1ce30d9cdeb11380656feb6ef55cac6227e1ea
will <will.qie@mail.utoronto.ca>
2021-06-30 03:45:16 -0400

    ff

--- BMMExt/test.py
+++ BMMExt/test.py
@@ -2,9 +2,9 @@ import torch
 import BMMExt
 
 if __name__ == "__main__":
-    a = torch.randn((4, 16, 3))
-    b = torch.randn((3, 3, 10))
-    c = torch.randn((3, 10))
-    s = torch.FloatTensor([16, 16, 32])
-    res = torch.empty((4, 16, 10))
-    BMMExt.forward(a, b, s, c, res, 4, 16)
+    a = torch.randn((4, 16, 3)).cuda()
+    b = torch.randn((3, 3, 10)).cuda()
+    c = torch.randn((3, 10)).cuda()
+    s = torch.FloatTensor([16, 16, 32]).cuda()
+    res = torch.empty((4, 16, 10)).cuda()
+    print(BMMExt.forward(a, b, s, c, res, 4, 16))
