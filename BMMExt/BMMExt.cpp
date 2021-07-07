074d785c56332e9b23df2fa3b6e3b6a90da3ded3
will <will.qie@mail.utoronto.ca>
2021-07-06 22:14:06 -0400

    start implement bias

--- BMMExt/BMMExt.cpp
+++ BMMExt/BMMExt.cpp
@@ -11,14 +11,21 @@ torch::Tensor BMMExt_forward(
     torch::Tensor input,
     torch::Tensor weights,
     torch::Tensor sizemap,
-    torch::Tensor result,int op_batch_num, int op_base_size) {
+    torch::Tensor result,
+    torch::Tensor bias,
+    torch::Tensor bias_multiplier,
+    int op_batch_num, int op_base_size) {
 
     static float** weight_arr = nullptr;
     static float** result_arr = nullptr;
     static float** input_arr = nullptr;
+    static float** bias_arr = nullptr;
+    static float** bias_multiplier_arr = nullptr;
     static float** weight_arr_cpu = nullptr;
     static float** result_arr_cpu = nullptr;
     static float** input_arr_cpu = nullptr;
+    static float** bias_arr_cpu = nullptr;
+    static float** bias_multiplier_cpu = nullptr;
     static int cur_batch_num = -1;
     static cublasStatus_t stat;
     static cublasHandle_t handle;
@@ -44,15 +51,27 @@ torch::Tensor BMMExt_forward(
             fprintf(stderr, "cudaMalloc fail\n");
             exit(1);
         }
+        if (cudaMalloc(&bias_arr, sizeof(float*)*op_batch_num) != cudaSuccess) {
+            fprintf(stderr, "cudaMalloc fail\n");
+            exit(1);
+        }
+        if (cudaMalloc(&bias_multiplier_arr, sizeof(float*)*op_batch_num) != cudaSuccess) {
+            fprintf(stderr, "cudaMalloc fail\n");
+            exit(1);
+        }
         weight_arr_cpu = (float**)malloc(sizeof(float*)*op_batch_num);
         result_arr_cpu = (float**)malloc(sizeof(float*)*op_batch_num);
         input_arr_cpu = (float**)malloc(sizeof(float*)*op_batch_num);
+        bias_arr_cpu = (float**)malloc(sizeof(float*)*op_batch_num);
+        bias_multiplier_cpu = (float**)malloc(sizeof(float*)*op_batch_num);
         cur_batch_num = op_batch_num;
     }
     
     float* weight_ptr = (float*)weights.data_ptr();
     float* result_ptr = (float*)result.data_ptr();
     float* input_ptr = (float*)input.data_ptr();
+    float* bias_ptr = (float*)bias.data_ptr();
+    float* bias_multiplier_ptr = (float*)bias_multiplier.data_ptr();
     auto weight_shape = weights.sizes();
     int num_features = weight_shape[2];
     int num_in = weight_shape[1];
@@ -62,7 +81,9 @@ torch::Tensor BMMExt_forward(
         for (int j = 0; j < ((int)(sizemap_ptr[i])) / op_base_size; j++) {
             input_arr_cpu[pos] = input_ptr + pos * op_base_size * num_in;
             weight_arr_cpu[pos] = weight_ptr + i * num_in * num_features;
+            bias_arr_cpu[pos] = bias_ptr + i * num_features;
             result_arr_cpu[pos] = result_ptr + pos * op_base_size * num_features;
+            bias_multiplier_cpu[pos] = bias_multiplier_ptr;
             pos++;
         }
     }
@@ -75,6 +96,14 @@ torch::Tensor BMMExt_forward(
         exit(1);
     }
     if (cudaMemcpy(input_arr, input_arr_cpu, sizeof(float*)*op_batch_num, cudaMemcpyHostToDevice) != cudaSuccess) {
+        fprintf(stderr, "Copy failed\n");
+        exit(1);
+    }
+    if (cudaMemcpy(bias_arr, bias_arr_cpu, sizeof(float*)*op_batch_num, cudaMemcpyHostToDevice) != cudaSuccess) {
+        fprintf(stderr, "Copy failed\n");
+        exit(1);
+    }
+    if (cudaMemcpy(bias_multiplier_arr, bias_multiplier_cpu, sizeof(float*)*op_batch_num, cudaMemcpyHostToDevice) != cudaSuccess) {
         fprintf(stderr, "Copy failed\n");
         exit(1);
     }
@@ -93,6 +122,21 @@ torch::Tensor BMMExt_forward(
         fprintf(stderr, "Cannot perform compute \n");
         exit(1);
     }
+
+    stat = cublasSgemmBatched(handle,
+    CUBLAS_OP_T, CUBLAS_OP_N,
+    num_features, op_base_size, 1,
+    &alpha,
+    bias_arr, num_features,
+    bias_multiplier_arr, 1,
+    &alpha, result_arr, num_features, op_batch_num);
+
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        fprintf(stderr, "Cannot perform compute \n");
+        exit(1);
+    }
+
+
     return result;
 }
 
--- BMMExt/test.py
+++ BMMExt/test.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     a = torch.rand((4, 128, 8)).cuda()
     b = torch.rand((3, 8, 3)).cuda()
     s = torch.FloatTensor([256, 128, 128])
+    bias = torch.randn((3, 3)).cuda()
+    bias_multiplier = torch.ones((128, )).cuda()
     res = torch.zeros((4, 128, 3)).cuda()
 
     b_ = b
-    res = BMMExt.op(a, b_, s, res, 4, 128)
-    res = BMMExt.op(a, b_, s, res, 4, 128)
+    res = BMMExt.op(a, b_, s, res, bias, bias_multiplier, 4, 128)
+    res = BMMExt.op(a, b_, s, res, bias, bias_multiplier, 4, 128)
     
     
     print(b_.is_contiguous())
@@ -19,8 +21,14 @@ if __name__ == "__main__":
     bb[1] = b_[0]
     bb[2] = b_[1]
     bb[3] = b_[2]
+
+    bias_ = torch.empty((4, 1, 3)).cuda()
+    bias_[0] = bias[0]
+    bias_[1] = bias[0]
+    bias_[2] = bias[1]
+    bias_[3] = bias[2]
     
-    expected = torch.bmm(a, bb)
+    expected = torch.baddbmm(bias_, a, bb)
     
     np.testing.assert_allclose(
                 res.cpu().numpy().flatten(),
